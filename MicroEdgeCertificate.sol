3203b2cd9b6c926cf0e5ad42eac9453ced713852
amefutd <166627066+amefutd@users.noreply.github.com>
2024-06-06 21:29:41 +0900

    Add files via upload
    first commit

---
+++ MicroEdgeCertificate.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MicroEdgeCertificate is ERC721URIStorage, Ownable {
+    uint256 public nextTokenId;
+    address public admin;
+
+    constructor() ERC721("MicroEdgeCertificate", "MEC") {
+        admin = msg.sender;
+    }
+
+    function mint(address to, string memory ipfsHash) external onlyOwner {
+        _safeMint(to, nextTokenId);
+        _setTokenURI(nextTokenId, string(abi.encodePacked("ipfs://", ipfsHash)));
+        nextTokenId++;
+    }
+
+    function revoke(uint256 tokenId) external onlyOwner {
+        _burn(tokenId);
+    }
+
+    function changeAdmin(address newAdmin) external onlyOwner {
+        admin = newAdmin;
+    }
+}
